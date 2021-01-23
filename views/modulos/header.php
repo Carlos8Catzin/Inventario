80a6095c909323eb1da1cfcbf85c662573f5b315
Carlos Alonso Vazquez Catzin <catzin10ficial@gmail.com>
2021-01-22 21:51:41 -0600

    commit

--- views/modulos/header.php
+++ views/modulos/header.php
@@ -9,7 +9,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
 	  <li class="nav-item">
-        <a class="nav-link font-weight-bold text-uppercase" href="inicio" role="button"><i class="fas fa-tshirt"></i> Novedades San Juan</a>
+        <a class="nav-link font-weight-bold text-uppercase" href="inicio" role="button">Novedades San Juan <i class="fas fa-tshirt"></i></a>
       </li>
     </ul>
         <ul class="navbar-nav ml-auto">
